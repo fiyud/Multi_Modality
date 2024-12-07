e26225744812f20a9d5e7da22c65a6db7b89958c
fiyud <chotala1113@gmail.com>
2024-12-07 23:45:47 +0700

    S2T

---
+++ S2T/__init__.py
@@ -0,0 +1,21 @@
+from transformers import pipeline
+
+import torch
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+pipe = pipeline("automatic-speech-recognition", model="H://Learning Files/Project AI/Aidemo/aidemo/S2T", device=device)
+
+def speech_to_text(audio_link):
+    text = pipe(audio_link)
+    return text
+
+def text_diarize_save(diarize_segment):
+    text_label = []
+    
+    for link in diarize_segment['link']:
+        text = speech_to_text(link)
+        diarize_segment['text'] = text
+        text_label.append(text)
+        
+    diarize_segment['text'] = text_label
+    return diarize_segment
